2180e217ff5749acf98f485f2cf69d4817df8b74
Qin3Yu <149878289+Qin3Yu@users.noreply.github.com>
2024-03-07 12:47:39 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ConversionToolsBox {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox_HEX = new System.Windows.Forms.TextBox();
+            this.button_conversion = new System.Windows.Forms.Button();
+            this.textBox_RGB = new System.Windows.Forms.TextBox();
+            this.textBox_HSL = new System.Windows.Forms.TextBox();
+            this.label_HEX = new System.Windows.Forms.Label();
+            this.label_RGB = new System.Windows.Forms.Label();
+            this.label_HSL = new System.Windows.Forms.Label();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.textBox_color = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_HEX
+            // 
+            this.textBox_HEX.Font = new System.Drawing.Font("Consolas", 14.25F);
+            this.textBox_HEX.Location = new System.Drawing.Point(97, 20);
+            this.textBox_HEX.Name = "textBox_HEX";
+            this.textBox_HEX.Size = new System.Drawing.Size(126, 30);
+            this.textBox_HEX.TabIndex = 0;
+            // 
+            // button_conversion
+            // 
+            this.button_conversion.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_conversion.Location = new System.Drawing.Point(97, 204);
+            this.button_conversion.Name = "button_conversion";
+            this.button_conversion.Size = new System.Drawing.Size(83, 37);
+            this.button_conversion.TabIndex = 1;
+            this.button_conversion.Text = "转换";
+            this.button_conversion.UseVisualStyleBackColor = true;
+            this.button_conversion.Click += new System.EventHandler(this.colorConversion);
+            // 
+            // textBox_RGB
+            // 
+            this.textBox_RGB.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_RGB.Location = new System.Drawing.Point(57, 67);
+            this.textBox_RGB.Name = "textBox_RGB";
+            this.textBox_RGB.Size = new System.Drawing.Size(166, 30);
+            this.textBox_RGB.TabIndex = 2;
+            // 
+            // textBox_HSL
+            // 
+            this.textBox_HSL.Font = new System.Drawing.Font("Consolas", 14.25F);
+            this.textBox_HSL.Location = new System.Drawing.Point(57, 115);
+            this.textBox_HSL.Name = "textBox_HSL";
+            this.textBox_HSL.Size = new System.Drawing.Size(166, 30);
+            this.textBox_HSL.TabIndex = 3;
+            // 
+            // label_HEX
+            // 
+            this.label_HEX.AutoSize = true;
+            this.label_HEX.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_HEX.Location = new System.Drawing.Point(11, 23);
+            this.label_HEX.Name = "label_HEX";
+            this.label_HEX.Size = new System.Drawing.Size(80, 22);
+            this.label_HEX.TabIndex = 4;
+            this.label_HEX.Text = "16#/HEX";
+            // 
+            // label_RGB
+            // 
+            this.label_RGB.AutoSize = true;
+            this.label_RGB.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_RGB.Location = new System.Drawing.Point(11, 70);
+            this.label_RGB.Name = "label_RGB";
+            this.label_RGB.Size = new System.Drawing.Size(40, 22);
+            this.label_RGB.TabIndex = 5;
+            this.label_RGB.Text = "RGB";
+            // 
+            // label_HSL
+            // 
+            this.label_HSL.AutoSize = true;
+            this.label_HSL.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_HSL.Location = new System.Drawing.Point(11, 118);
+            this.label_HSL.Name = "label_HSL";
+            this.label_HSL.Size = new System.Drawing.Size(40, 22);
+            this.label_HSL.TabIndex = 6;
+            this.label_HSL.Text = "HSL";
+            // 
+            // button_clear
+            // 
+            this.button_clear.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.button_clear.Location = new System.Drawing.Point(244, 204);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(83, 37);
+            this.button_clear.TabIndex = 7;
+            this.button_clear.Text = "清除";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.colorClear);
+            // 
+            // textBox_color
+            // 
+            this.textBox_color.Font = new System.Drawing.Font("宋体", 80F);
+            this.textBox_color.Location = new System.Drawing.Point(256, 18);
+            this.textBox_color.Name = "textBox_color";
+            this.textBox_color.Size = new System.Drawing.Size(128, 129);
+            this.textBox_color.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(104, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 21);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "PS.多次输入前请先点击清除";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 258);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_color);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.label_HSL);
+            this.Controls.Add(this.label_RGB);
+            this.Controls.Add(this.label_HEX);
+            this.Controls.Add(this.textBox_HSL);
+            this.Controls.Add(this.textBox_RGB);
+            this.Controls.Add(this.button_conversion);
+            this.Controls.Add(this.textBox_HEX);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "颜色转换器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBox_HEX;
+        private System.Windows.Forms.Button button_conversion;
+        private System.Windows.Forms.TextBox textBox_RGB;
+        private System.Windows.Forms.TextBox textBox_HSL;
+        private System.Windows.Forms.Label label_HEX;
+        private System.Windows.Forms.Label label_RGB;
+        private System.Windows.Forms.Label label_HSL;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.TextBox textBox_color;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ConversionToolsBox {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        public void colorConversion(object sender, EventArgs e) /* 点击转换后触发 */ {
+
+            string color_HEX, color_RGB="255,255,255", color_HSL;
+
+            // 根据HEX转换
+            if (textBox_HEX.Text != "") {
+                color_HEX = textBox_HEX.Text.Trim().Replace("#", ""); // 去除#号
+                if (color_HEX.Length == 6) {
+
+                    // 转换为RGB色
+                    color_RGB = string.Join(",", HEXtoRGB(color_HEX));
+                    textBox_RGB.Text = color_RGB;
+
+                    // 转换为HSL色
+                    color_HSL = string.Join(",", RGBtoHSL(HEXtoRGB(color_HEX)));
+                    textBox_HSL.Text = color_HSL;
+                }
+            }
+
+            // 根据RGB转换
+            else if (textBox_RGB.Text != "") {
+
+                color_RGB = textBox_RGB.Text;
+                string[] color_RGB_s = color_RGB.Split(',');
+
+                int R = int.Parse(color_RGB_s[0]);
+                int G = int.Parse(color_RGB_s[1]);
+                int B = int.Parse(color_RGB_s[2]);
+                int[] RGB = {R, G, B};
+
+                // 转换为HEX色
+                color_HEX = RGBtoHEX(RGB);
+                textBox_HEX.Text = "#" + color_HEX;
+
+                // 转换为HSL色
+                color_HSL = string.Join(",", RGBtoHSL(HEXtoRGB(color_HEX)));
+                textBox_HSL.Text = color_HSL;
+
+            }
+
+            // 根据HSL转换
+            else if (textBox_HSL.Text != "") {
+
+                color_HSL = textBox_HSL.Text;
+                string[] color_HSL_s = color_HSL.Split(',');
+
+                float H = float.Parse(color_HSL_s[0]);
+                float S = float.Parse(color_HSL_s[1]);
+                float L = float.Parse(color_HSL_s[2]);
+                float[] HSL= {H, S, L};
+
+                // 转换为RGB色
+                color_RGB=string.Join(",", HSLtoRGB(HSL));
+                textBox_RGB.Text = color_RGB;
+
+                // 转换为HEX色
+                color_HEX=RGBtoHEX(HSLtoRGB(HSL));
+                textBox_HEX.Text= "#"+ color_HEX;
+            }
+
+            string[] color_RGBs = color_RGB.Split(',');
+            int[] textBoxRGB = { int.Parse(color_RGBs[0]), int.Parse(color_RGBs[1]), int.Parse(color_RGBs[2]) };
+            Color rgbColor = Color.FromArgb(textBoxRGB[0], textBoxRGB[1], textBoxRGB[2]);
+            textBox_color.BackColor = rgbColor;
+
+        }
+
+        public void colorClear(object sender,EventArgs e) /* 点击清除后触发 */ {
+            textBox_HEX.Text = "";
+            textBox_RGB.Text = "";
+            textBox_HSL.Text = "";
+            Color rgbColor = Color.FromArgb(255, 255, 255);
+            textBox_color.BackColor = rgbColor;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) /* 窗体属性控制 */ {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;  // 设置窗口边框为固定大小
+            this.MaximizeBox = false;  // 禁用最大化按钮
+            this.MinimizeBox = false;  // 禁用最小化按钮
+            textBox_color.ReadOnly = true;
+
+            Color rgbColor = Color.FromArgb(255, 255, 255);
+            textBox_color.BackColor = rgbColor;
+
+        }
+
+        public int[] HEXtoRGB (string color_HEX) /* 接收一个去除 # 的HEX色值，返回RGB数组 */ {
+            int R = Convert.ToInt32(color_HEX.Substring(0, 2), 16);
+            int G = Convert.ToInt32(color_HEX.Substring(2, 2), 16);
+            int B = Convert.ToInt32(color_HEX.Substring(4, 2), 16);
+            return new int[] { R, G, B };
+        }
+
+        public float[] RGBtoHSL(int[] nums_RGB) /* 接收一个0~255的RGB数组，返回规格为整形和两位小数的HSL数组 */ {
+            float R = nums_RGB[0] / 255f;
+            float G = nums_RGB[1] / 255f;
+            float B = nums_RGB[2] / 255f;
+            float max = Math.Max(R, Math.Max(G, B));
+            float min = Math.Min(R, Math.Min(G, B));
+            float H = 0;
+            if (max == min)  H = 0;
+            else if (max == R)  H = 60 * (G - B) / (max - min);
+            else if (max == G)  H = 60 * (B - R) / (max - min) + 120;
+            else if (max == B)  H = 60 * (R - G) / (max - min) + 240;
+            if (H < 0)  H += 360;
+            float L = (max + min) / 2;
+            float S = 0;
+            if (max == min)  S = 0;
+            else if (0 < L && L < 0.5f)  S = (max - min) / (2 * L);
+            else if (L >= 0.5f)  S = (max - min) / (2 - 2 * L);
+            return new float[] { (float)Math.Round(H), (float)Math.Round(S, 2), (float)Math.Round(L, 2) };
+        }
+
+        public string RGBtoHEX(int[] nums_RGB) /* 接收一个0~255的RGB数组，返回不带#的六位HEX色值字符串 */ {
+            string R = nums_RGB[0].ToString("X").PadLeft(2, '0');
+            string G = nums_RGB[1].ToString("X").PadLeft(2, '0');
+            string B = nums_RGB[2].ToString("X").PadLeft(2, '0');
+            return R + G + B;
+        }
+
+        public int[] HSLtoRGB(float[] nums_HSL) /* 接收一个规格为整形和两位小数的HSL数组，返回一个0~255的RGB数组 */ {
+            float precision = 0.00001f;
+            float hue = nums_HSL[0];
+            float sat = nums_HSL[1];
+            float light = nums_HSL[2];
+            int[] RGB = new int[3];
+
+            if (sat < precision) // sat == 0
+            {
+                RGB[0] = (int)(light * 255);
+                RGB[1] = (int)(light * 255);
+                RGB[2] = (int)(light * 255);
+            }
+            else {
+                float q, p, hk;
+
+                if (light < 0.5f) { q = light * (1 + sat); }
+                else { q = light + sat - (light * sat); }
+                p = 2.0f * light - q;
+                hk = hue / 360.0f;
+                RGB[0] = (int)(ToColor_IN_HSLtoRGB(p, q, hk + 0.33333f) * 255);
+                RGB[1] = (int)(ToColor_IN_HSLtoRGB(p, q, hk) * 255);
+                RGB[2] = (int)(ToColor_IN_HSLtoRGB(p, q, hk - 0.33333f) * 255);
+            }
+            return RGB;
+        }
+
+        private static float ToColor_IN_HSLtoRGB(float p, float q, float t) /* 辅助HSLtoRGB函数 */ {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1.0f / 6.0f) return p + (q - p) * 6 * t;
+            if (t < 0.5f) return q;
+            if (t < 2.0f / 3.0f) return p + (q - p) * (2.0f / 3.0f - t) * 6;
+            return p;
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
